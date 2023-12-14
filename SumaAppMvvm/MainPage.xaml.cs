8d2c210de5b7ca59620f0f455740222d9db021b6
Yoleny <yoleny.salme28@gmail.com>
2023-12-13 20:03:42 -0600

    Agregar archivos de proyecto.

---
+++ SumaAppMvvm/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SumaAppMvvm
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private double resultado;
+        public double Resultado
+        {
+            get { return resultado; }
+            set { resultado = value; NotifyPropertyChanged(); }
+        }
+
+        public Command SumarCommand { get; }
+        public Command LimpiarCommand { get; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            SumarCommand = new Command(Sumar, ValidarCampos);
+            LimpiarCommand = new Command(Limpiar);
+
+            BindingContext = this;
+        }
+
+        private bool ValidarCampos()
+        {
+            return !string.IsNullOrWhiteSpace(valor1Entry.Text) && !string.IsNullOrWhiteSpace(valor2Entry.Text);
+        }
+
+        private void Sumar()
+        {
+            double valor1 = Convert.ToDouble(valor1Entry.Text);
+            double valor2 = Convert.ToDouble(valor2Entry.Text);
+            Resultado = valor1 + valor2;
+
+            resultadoLabel.Text = Resultado.ToString();
+        }
+
+        private void Limpiar()
+        {
+            valor1Entry.Text = string.Empty;
+            valor2Entry.Text = string.Empty;
+            resultadoLabel.Text = string.Empty;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ SumaAppMvvm/ViewModels/MainViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui;
+
+namespace SumaAppMvvm
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private double value1;
+        private double value2;
+        private double result;
+        private string resultMessage;
+
+        public double Value1
+        {
+            get { return value1; }
+            set
+            {
+                if (value1 != value)
+                {
+                    value1 = value;
+                    OnPropertyChanged(nameof(Value1));
+                }
+            }
+        }
+
+        public double Value2
+        {
+            get { return value2; }
+            set
+            {
+                if (value2 != value)
+                {
+                    value2 = value;
+                    OnPropertyChanged(nameof(Value2));
+                }
+            }
+        }
+
+        public double Result
+        {
+            get { return result; }
+            set
+            {
+                if (result != value)
+                {
+                    result = value;
+                    OnPropertyChanged(nameof(Result));
+                }
+            }
+        }
+
+        public string ResultMessage
+        {
+            get { return resultMessage; }
+            set
+            {
+                if (resultMessage != value)
+                {
+                    resultMessage = value;
+                    OnPropertyChanged(nameof(ResultMessage));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void SumValues()
+        {
+            if (Value1 == 0 || Value2 == 0)
+            {
+                ResultMessage = "Error: Ambos valores deben ser mayores a cero.";
+                return;
+            }
+
+            Result = Value1 + Value2;
+            ResultMessage = $"El resultado es: {Result}";
+        }
+
+        public void ClearFields()
+        {
+            Value1 = 0;
+            Value2 = 0;
+            Result = 0;
+            ResultMessage = "";
+        }
+    }
+}
